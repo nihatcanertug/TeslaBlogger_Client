52323dfb98db60ec43a7b19dcdce165eab3e5fab
nihatcan ertuğ <nihatcanertug@hotmail.com>
2021-02-23 16:41:09 +0300

    Add project files.

---
+++ TeslaBlogger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeslaBlogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
